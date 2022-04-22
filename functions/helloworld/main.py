51ed026074d9d911c91bb12be81719379b103520
Ali Mohammadian <alirsgp@gmail.com>
2022-04-21 18:19:31 -0700

    change

--- functions/helloworld/main.py
+++ functions/helloworld/main.py
@@ -60,6 +60,7 @@ def hello_http(request):
     """
     request_json = request.get_json(silent=True)
     request_args = request.args
+    
     print(request_json)
 
     if request_json and 'name' in request_json:
